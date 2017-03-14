b527581deb07b3c75edf5384b2764f9b9b532fee
Dave Brophy <dave@brophy.uk>
2017-03-14 09:28:32 +0100

    Update generated-editor.go

--- editor/generated-editor.go
+++ editor/generated-editor.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	_ "github.com/davelondon/ke_common/images"
-	_ "github.com/davelondon/ke_common/images/types"
-	_ "github.com/davelondon/ke_common/units"
-	_ "github.com/davelondon/ke_common/units/types"
-	_ "github.com/davelondon/ke_common/words"
-	_ "github.com/davelondon/ke_common/words/types"
-	_ "github.com/davelondon/ke_site"
-	_ "github.com/davelondon/ke_site/types"
+	_ "github.com/dave/ke_common/images"
+	_ "github.com/dave/ke_common/images/types"
+	_ "github.com/dave/ke_common/units"
+	_ "github.com/dave/ke_common/units/types"
+	_ "github.com/dave/ke_common/words"
+	_ "github.com/dave/ke_common/words/types"
+	_ "github.com/dave/ke_site"
+	_ "github.com/dave/ke_site/types"
 	"kego.io/process/editor"
 	_ "kego.io/system"
 	_ "kego.io/system/types"
 )
 
 func main() {
-	editor.Edit("github.com/davelondon/ke_site")
+	editor.Edit("github.com/dave/ke_site")
 }
